fa5589764289e9060d9e2075fc88ec3a000304e1
alitaee <ali.taee.72@gmail.com>
2021-07-27 21:51:44 +0430

    Setup prettier and eslint

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "../assets/logo.svg";
-import "../styles/App.scss";
+import React from 'react'
+import logo from '../assets/logo.svg'
+import '../styles/App.scss'
 
 function App() {
   return (
@@ -20,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
